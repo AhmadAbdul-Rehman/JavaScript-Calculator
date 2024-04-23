1fd006fc3f07a99c7174b50bbd87517e6f827e93
Ahmad <ahmad.coder.dps2610@gmail.com>
2024-04-23 09:04:48 -0700

    user-selection

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 let display = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
-
 let buttnsArray = Array.from(buttons);
-
 let string = "";
-
 buttnsArray.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        if(e.target.innerHTML == "DEL"){
-            string = string.substring(0, string.length-1)
+        if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1)
             display.value = string;
         }
-        else if(e.target.innerHTML == "AC"){
+        else if (e.target.innerHTML == "AC") {
             string = "";
             display.value = string;
         }
-        else if(e.target.innerHTML == "="){
+        else if (e.target.innerHTML == "=") {
             string = eval(string);
             display.value = string;
         }
-        else{
+        else {
             string += e.target.innerHTML;
             display.value = string;
         }
